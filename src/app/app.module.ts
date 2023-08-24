bbdced11e727194384fc46bc7b7db68959325a53
kumarnivi <nivethikashivakumar56@gmail.com>
2023-08-24 23:21:17 +0530

    add Section02 and Footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
 import { Section01Component } from './section01/section01.component';
 import { Section02Component } from './section02/section02.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
     Section01Component,
-    Section02Component
+    Section02Component,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
