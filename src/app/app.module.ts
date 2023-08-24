5638d0f7ca833f8580ed3e5701bd14d6e68c353e
kumarnivi <nivethikashivakumar56@gmail.com>
2023-08-24 21:06:45 +0530

    add Section01

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
+import { Section01Component } from './section01/section01.component';
+import { Section02Component } from './section02/section02.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavComponent
+    NavComponent,
+    Section01Component,
+    Section02Component
   ],
   imports: [
     BrowserModule,
